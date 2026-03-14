62454cad3fe27ccb60627caacf5222aa817f8a32
josematheww <mathewjose1175@gamil.com>
2026-03-14 17:46:12 +0530

    new change

--- clg_programss.py
+++ clg_programss.py
@@ -1,5 +1,15 @@
+
+#GCD CALCUALTION 
 def gcd(a,b):
     while b:
         a,b=b,a%b
     return a
 print(gcd(98,30))
+
+def linear_search(arr,key):
+    for i in arr:
+        if i == key:
+            return i
+    return -1
+
+
